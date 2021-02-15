6e48bd822078fb69101ae8ded3a44d31b556888f
Massimo Candela <massimo@us.ntt.net>
2021-02-15 23:59:32 +0100

    introduced methods to export data plus some refactoring

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const LongestPrefixMatch = function () {
     };
 
     this.reset = () => {
-        this.roas = {
+        this.data = {
             v4: new RadixTrie(),
             v6: new RadixTrie()
         };
@@ -23,9 +23,9 @@ const LongestPrefixMatch = function () {
 
     this._getMatch = (binaryNetmask, af) => {
         if (af === 4) {
-            return this.roas.v4.get(binaryNetmask.slice(0, this.keySizes.v4));
+            return this.data.v4.get(binaryNetmask.slice(0, this.keySizes.v4));
         } else {
-            return this.roas.v6.get(binaryNetmask.slice(0, this.keySizes.v6));
+            return this.data.v6.get(binaryNetmask.slice(0, this.keySizes.v6));
         }
     };
 
@@ -36,20 +36,28 @@ const LongestPrefixMatch = function () {
         return this._addPrefix(binaryPrefix, af, data);
     };
 
+    this.getData = () => {
+        return this.data;
+    };
+
+    this.toArray = () => {
+        return [].concat.apply([],[...this.data.v4.values(), ...this.data.v6.values()]);
+    };
+
     this._addPrefix = (binaryPrefix, af, data) => {
 
         if (af === 4) {
             const key = binaryPrefix.slice(0, this.keySizes.v4);
-            if (!this.roas.v4.has(key)) {
-                this.roas.v4.add(key, []);
+            if (!this.data.v4.has(key)) {
+                this.data.v4.add(key, []);
             }
-            this.roas.v4.get(key).push(data);
+            this.data.v4.get(key).push(data);
         } else {
             const key = binaryPrefix.slice(0, this.keySizes.v6);
-            if (!this.roas.v6.has(key)) {
-                this.roas.v6.add(key, []);
+            if (!this.data.v6.has(key)) {
+                this.data.v6.add(key, []);
             }
-            this.roas.v6.get(key).push(data);
+            this.data.v6.get(key).push(data);
         }
     };
 
