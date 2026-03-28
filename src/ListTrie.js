5becc636fc24d6e1da12b5767de0bf669c96e055
Massimo Candela <massimo@us.ntt.net>
2026-03-28 13:10:08 +0100

    reduced memory usage with prototypes

--- src/ListTrie.js
+++ src/ListTrie.js
@@ -1,154 +1,153 @@
 const ListTrie = function () {
     this.index = {};
-
-    this._newNode = () => {
-        return {
-            zero: null,
-            one: null,
-            values: null
-        };
-    };
-
     this.root = this._newNode();
+}
 
-    this._walk = (visitor) => {
-        const stack = [{ node: this.root, key: "" }];
-
-        while (stack.length > 0) {
-            const current = stack.pop();
-            const node = current.node;
-
-            if (node.values) {
-                visitor(current.key, node.values);
-            }
-
-            if (node.one) {
-                stack.push({ node: node.one, key: `${current.key}1` });
-            }
-
-            if (node.zero) {
-                stack.push({ node: node.zero, key: `${current.key}0` });
-            }
-        }
+ListTrie.prototype._newNode = function () {
+    return {
+        zero: null,
+        one: null,
+        values: null
     };
+};
 
-    this.reset = () => {
-        this.index = {};
-        this.root = this._newNode();
+ListTrie.prototype._walk = function (visitor) {
+    const stack = [{ node: this.root, key: "" }];
+
+    while (stack.length > 0) {
+        const current = stack.pop();
+        const node = current.node;
+
+        if (node.values) {
+            visitor(current.key, node.values);
+        }
+
+        if (node.one) {
+            stack.push({ node: node.one, key: `${current.key}1` });
+        }
+
+        if (node.zero) {
+            stack.push({ node: node.zero, key: `${current.key}0` });
+        }
+    }
+};
+
+ListTrie.prototype.reset = function () {
+    this.index = {};
+    this.root = this._newNode();
+}
+
+ListTrie.prototype.add = function (key, value) {
+    let node = this.root;
+
+    for (let n = 0; n < key.length; n++) {
+        const branch = key[n] === '0' ? 'zero' : 'one';
+
+        if (!node[branch]) {
+            node[branch] = this._newNode();
+        }
+
+        node = node[branch];
     }
 
-    this.add = (key, value) => {
-        let node = this.root;
+    if (node.values) {
+        node.values.push(value);
+    } else {
+        node.values = [value];
+    }
+}
 
-        for (let n = 0; n < key.length; n++) {
-            const branch = key[n] === '0' ? 'zero' : 'one';
+ListTrie.prototype.rebuildIndex = function () {
+    const index = {};
 
-            if (!node[branch]) {
-                node[branch] = this._newNode();
-            }
+    this._walk((key, values) => {
+        index[key] = values;
+    });
 
-            node = node[branch];
+    this.index = index;
+    return this.index;
+}
+
+ListTrie.prototype.get = function (key, all) {
+    let node = this.root;
+    let best = null;
+    const matches = all ? [] : null;
+
+    for (let n = 0; n < key.length; n++) {
+        node = node[key[n] === '0' ? 'zero' : 'one'];
+
+        if (!node) {
+            break;
         }
 
         if (node.values) {
-            node.values.push(value);
-        } else {
-            node.values = [value];
+            if (all) {
+                matches.push(node.values);
+            } else {
+                best = node.values;
+            }
         }
     }
 
-    this.rebuildIndex = () => {
-        const index = {};
-
-        this._walk((key, values) => {
-            index[key] = values;
-        });
-
-        this.index = index;
-        return this.index;
+    if (!all) {
+        return best || [];
     }
 
-    this.get = (key, all) => {
-        let node = this.root;
-        let best = null;
-        const matches = all ? [] : null;
-
-        for (let n = 0; n < key.length; n++) {
-            node = node[key[n] === '0' ? 'zero' : 'one'];
-
-            if (!node) {
-                break;
-            }
-
-            if (node.values) {
-                if (all) {
-                    matches.push(node.values);
-                } else {
-                    best = node.values;
-                }
-            }
-        }
-
-        if (!all) {
-            return best || [];
-        }
-
-        if (matches.length === 0) {
-            return [];
-        }
-
-        const results = [];
-        for (let i = matches.length - 1; i >= 0; i--) {
-            const group = matches[i];
-            for (let j = 0; j < group.length; j++) {
-                results.push(group[j]);
-            }
-        }
-
-        return results;
-    }
-
-    this.getLessSpecific = (key) => {
-        let node = this.root;
-
-        for (let n = 0; n < key.length; n++) {
-            node = node[key[n] === '0' ? 'zero' : 'one'];
-
-            if (!node) {
-                break;
-            }
-
-            if (node.values) {
-                return node.values;
-            }
-        }
-
+    if (matches.length === 0) {
         return [];
     }
 
-    this.at = (key) => {
-        let node = this.root;
+    const results = [];
+    for (let i = matches.length - 1; i >= 0; i--) {
+        const group = matches[i];
+        for (let j = 0; j < group.length; j++) {
+            results.push(group[j]);
+        }
+    }
 
-        for (let n = 0; n < key.length; n++) {
-            node = node[key[n] === '0' ? 'zero' : 'one'];
+    return results;
+}
 
-            if (!node) {
-                return undefined;
-            }
+ListTrie.prototype.getLessSpecific = function (key) {
+    let node = this.root;
+
+    for (let n = 0; n < key.length; n++) {
+        node = node[key[n] === '0' ? 'zero' : 'one'];
+
+        if (!node) {
+            break;
         }
 
-        return node.values;
+        if (node.values) {
+            return node.values;
+        }
     }
 
-    this.values = () => {
-        const allValues = [];
+    return [];
+}
 
-        this._walk((key, values) => {
-            allValues.push(values);
-        });
+ListTrie.prototype.at = function (key) {
+    let node = this.root;
 
-        return allValues;
+    for (let n = 0; n < key.length; n++) {
+        node = node[key[n] === '0' ? 'zero' : 'one'];
+
+        if (!node) {
+            return undefined;
+        }
     }
+
+    return node.values;
+}
+
+ListTrie.prototype.values = function () {
+    const allValues = [];
+
+    this._walk((key, values) => {
+        allValues.push(values);
+    });
+
+    return allValues;
 }
 
 module.exports = ListTrie;
--- src/index.js
+++ src/index.js
@@ -3,72 +3,71 @@ const ListTrie = require('./ListTrie');
 
 const LongestPrefixMatch = function (params={}) {
     this.length = 0;
-
-    this.reset = () => {
-        this.length = 0;
-        this.data = {
-            v4: new ListTrie(),
-            v6: new ListTrie()
-        };
-    };
-
-    this.getMatch = (prefix, all) => {
-        const af = ip.getAddressFamily(prefix);
-        const binaryNetmask = ip.applyNetmask(prefix, af);
-
-        return this._getMatch(binaryNetmask, af, all).map(i => i.data);
-    };
-
-    this.getLessSpecificMatch = (prefix) => {
-        const af = ip.getAddressFamily(prefix);
-        const binaryNetmask = ip.applyNetmask(prefix, af);
-
-        return this._getLessSpecificMatch(binaryNetmask, af).map(i => i.data);
-    };
-
-    this._getLessSpecificMatch = (binaryNetmask, af) => {
-
-        const afKey = `v${af}`;
-        return this.data[afKey].getLessSpecific(binaryNetmask);
-    };
-
-    this._getMatch = (binaryNetmask, af, all) => {
-
-        const afKey = `v${af}`;
-        return this.data[afKey].get(binaryNetmask, all);
-    };
-
-    this.addPrefix = (prefix, payload) => {
-        const af = ip.getAddressFamily(prefix);
-        const binaryPrefix = ip.applyNetmask(prefix, af);
-
-        return this._addPrefix(binaryPrefix, af, payload);
-    };
-
-    this.getData = () => {
-        this.data.v4.rebuildIndex();
-        this.data.v6.rebuildIndex();
-        return this.data;
-    };
-
-    this.toArray = () => {
-        return [...this.data.v4.values(), ...this.data.v6.values()].flat().map(i => i.data);
-    };
-
-    this._addPrefix = (binaryPrefix, af, payload) => {
-        this.length++;
-        const data = {
-            data: payload,
-            binaryPrefix,
-            length: binaryPrefix.length
-        }
-
-        const afKey = `v${af}`;
-
-        this.data[afKey].add(binaryPrefix, data);
-    };
-
     this.reset();
+};
+
+LongestPrefixMatch.prototype.reset = function () {
+    this.length = 0;
+    this.data = {
+        v4: new ListTrie(),
+        v6: new ListTrie()
+    };
+};
+
+LongestPrefixMatch.prototype.getMatch = function (prefix, all) {
+    const af = ip.getAddressFamily(prefix);
+    const binaryNetmask = ip.applyNetmask(prefix, af);
+
+    return this._getMatch(binaryNetmask, af, all).map(i => i.data);
+};
+
+LongestPrefixMatch.prototype.getLessSpecificMatch = function (prefix) {
+    const af = ip.getAddressFamily(prefix);
+    const binaryNetmask = ip.applyNetmask(prefix, af);
+
+    return this._getLessSpecificMatch(binaryNetmask, af).map(i => i.data);
+};
+
+LongestPrefixMatch.prototype._getLessSpecificMatch = function (binaryNetmask, af) {
+
+    const afKey = `v${af}`;
+    return this.data[afKey].getLessSpecific(binaryNetmask);
+};
+
+LongestPrefixMatch.prototype._getMatch = function (binaryNetmask, af, all) {
+
+    const afKey = `v${af}`;
+    return this.data[afKey].get(binaryNetmask, all);
+};
+
+LongestPrefixMatch.prototype.addPrefix = function (prefix, payload) {
+    const af = ip.getAddressFamily(prefix);
+    const binaryPrefix = ip.applyNetmask(prefix, af);
+
+    return this._addPrefix(binaryPrefix, af, payload);
+};
+
+LongestPrefixMatch.prototype.getData = function () {
+    this.data.v4.rebuildIndex();
+    this.data.v6.rebuildIndex();
+    return this.data;
+};
+
+LongestPrefixMatch.prototype.toArray = function () {
+    return [...this.data.v4.values(), ...this.data.v6.values()].flat().map(i => i.data);
+};
+
+LongestPrefixMatch.prototype._addPrefix = function (binaryPrefix, af, payload) {
+    this.length++;
+    const data = {
+        data: payload,
+        binaryPrefix,
+        length: binaryPrefix.length
+    }
+
+    const afKey = `v${af}`;
+
+    this.data[afKey].add(binaryPrefix, data);
 };
 
 module.exports = LongestPrefixMatch;
